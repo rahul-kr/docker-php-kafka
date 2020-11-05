5ed5d1bf4548301aba8c95a462115b8c517b6d66
Rahul Kumar <rahul.kumar@srijan.net>
2020-11-05 11:43:01 +0530

    initial commit

---
+++ producer.php
@@ -0,0 +1,22 @@
+<?php
+// phpinfo();die('ad');
+
+$producer = new \RdKafka\Producer();
+$producer->setLogLevel(LOG_DEBUG);
+
+if ($producer->addBrokers("kafka:9092") < 1) {
+    echo "Failed adding brokers\n";
+    exit;
+}
+
+$topic = $producer->newTopic("watchdog");
+// print_r($producer);die('sfsd');
+
+if (!$producer->getMetadata(false, $topic, 2000)) {
+    echo "Failed to get metadata, is broker down?\n";
+    exit;
+}
+
+$topic->produce(RD_KAFKA_PARTITION_UA, 0, $_SERVER['QUERY_STRING']);
+
+echo "Message published\n";
